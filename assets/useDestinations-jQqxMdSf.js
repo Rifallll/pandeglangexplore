bed4a8c401090f3abafa6529916e60b3f1a77d8f
Rifallll <125524926+Rifallll@users.noreply.github.com>
2026-02-17 01:29:06 +0000

    Deploying to gh-pages from @ Rifallll/pandeglangexplore@ce458d25781962c7ed572570a1fa31e6491b904b 🚀

---
+++ assets/useDestinations-jQqxMdSf.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-BEzj5P7e.js";import{a}from"./destinations-DV2L9mzu.js";const c=()=>{const[s,n]=t.useState(a),[e,r]=t.useState(!1),[o,i]=t.useState(null);return{destinations:s,loading:e,error:o}};export{c as u};
